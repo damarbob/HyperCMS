4141fe06925a5324355af6af943413531bab13ee
damar <ibn.damr@gmail.com>
2025-05-09 16:54:43 +0700

    Optimized Hyper configuration usage across multiple instances.

--- app/Views/auth/layout/page.php
+++ app/Views/auth/layout/page.php
@@ -10,7 +10,7 @@ use Config\Hyper;
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= isset($title) ? $title : (new Hyper)->appName ?></title>
+    <title><?= isset($title) ? $title : config(Hyper::class)->appName ?></title>
 
     <!-- Styles & icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
