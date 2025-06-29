b64014a01d507170b004d8d668c1364a9c2710e3
damar <ibn.damr@gmail.com>
2025-06-29 08:55:08 +0700

    Enhanced Paging System settings with one-click setup and improved the UI.

--- modules/PagingSystem/Language/en/PagingSystem.php
+++ modules/PagingSystem/Language/en/PagingSystem.php
@@ -10,13 +10,6 @@ return [
 
     'openWithEditor' => 'Open with Editor',
 
-    'primary' => 'Primary',
-    'chosenPrimaryModelWillBeRoutedToTheFrontend' => "The chosen primary model will be routed to the frontend without a path prefix. For instance, a model named <b>Page</b> with an entry name/URL of <b>products</b> will be routed to <i>" . base_url('products') . "</i> instead of <i>" . base_url('page/products') . "</i>.",
-    'assets' => 'Assets',
-    'selectedModelWillServeAsPrimaryModelForServingAssets' => 'The selected model will serve as the primary model for serving assets on the frontend.',
-    'meta' => 'Meta',
-    'selectedMetaModelWillBeUsedToInject' => 'The selected meta model will be used to inject meta tags into pages.',
-
     'blocks' => 'Blocks',
     'layers' => 'Layers',
     'search' => 'Search',
@@ -46,5 +39,18 @@ return [
     'desktop' => 'Desktop',
     'tablet' => 'Tablet',
     'mobilePortrait' => 'Mobile Portrait',
-    'MobileLandscape' => 'Mobile Desktop',
+    'mobileLandscape' => 'Mobile Desktop',
+
+    // Settings
+    'toEnsureBestExperienceInPageEditor' => 'To ensure the best experience in the Page Editor, you need the following model.',
+    'oneClickSetup' => 'One-click Setup',
+    'setUpModelsSuccessfully' => "Set up models successfully!",
+
+    'primary' => 'Primary',
+    'page' => 'Page',
+    'chosenPrimaryModelWillBeRoutedToTheFrontend' => "The chosen primary model will be routed to the frontend without a path prefix. For instance, a model named <b>Page</b> with an entry name/URL of <b>products</b> will be routed to <i>" . base_url('products') . "</i> instead of <i>" . base_url('page/products') . "</i>.",
+    'assets' => 'Assets',
+    'selectedModelWillServeAsPrimaryModelForServingAssets' => 'The selected model will serve as the primary model for serving assets on the frontend.',
+    'meta' => 'Meta',
+    'selectedMetaModelWillBeUsedToInject' => 'The selected meta model will be used to inject meta tags into pages.',
 ];
--- modules/PagingSystem/Views/Admin/settings.php
+++ modules/PagingSystem/Views/Admin/settings.php
@@ -14,77 +14,122 @@ $metaModelError = validation_show_error('paging_system_meta_model_id');
     <p class="subtitle">
         <?= lang('PagingSystem.moduleDescription') ?>
     </p>
-    <form action="<?= base_url('admin/settings/paging-system/update') ?>" method="POST">
-        <?= csrf_field() ?>
 
-        <!-- Primary model selection -->
-        <div class="field">
-            <label class="label"><?= lang('PagingSystem.primary') ?></label>
-            <div class="control">
-                <div class="select">
-                    <select name="paging_system_primary_model_id">
-                        <?php for ($i = 0; $i < count($pagingSystemEligibleModelNames); $i++): ?>
-                            <option value="<?= $pagingSystemEligibleModelIds[$i] ?>"><?= $pagingSystemEligibleModelNames[$i] ?></option>
-                        <?php endfor; ?>
-                    </select>
+    <?php if (!$eligibleModelExists || !$eligibleAssetsModelExists): ?>
+        <div class="notification">
+            <p><?= lang('PagingSystem.toEnsureBestExperienceInPageEditor') ?></p>
+            <ul>
+                <?php if (!$eligibleModelExists): ?>
+                    <li>
+                        <span class="icon">
+                            <i class="fas fa-file"></i>
+                        </span>
+                        <span>
+                            <?= lang('PagingSystem.page') ?>
+                        </span>
+                    </li>
+                <?php endif ?>
+                <?php if (!$eligibleAssetsModelExists): ?>
+                    <li>
+                        <span class="icon">
+                            <i class="fas fa-file-code"></i>
+                        </span>
+                        <span>
+                            <?= lang('PagingSystem.assets') ?>
+                        </span>
+                    </li>
+                <?php endif ?>
+            </ul>
+            <a class="button is-primary mt-2" href="<?= base_url('admin/settings/paging-system?setup=true') ?>">
+                <span class="icon">
+                    <i class="fas fa-check"></i>
+                </span>
+                <span>
+                    <?= lang('PagingSystem.oneClickSetup') ?>
+                </span>
+            </a>
+        </div>
+    <?php endif ?>
+
+    <?php if ($eligibleModelExists || $eligibleAssetsModelExists || $eligibleMetaModelExists): ?>
+        <form action="<?= base_url('admin/settings/paging-system/update') ?>" method="POST">
+            <?= csrf_field() ?>
+
+            <?php if ($eligibleModelExists): ?>
+                <!-- Primary model selection -->
+                <div class="field">
+                    <label class="label"><?= lang('PagingSystem.primary') ?></label>
+                    <div class="control">
+                        <div class="select">
+                            <select name="paging_system_primary_model_id">
+                                <?php for ($i = 0; $i < count($pagingSystemEligibleModelNames); $i++): ?>
+                                    <option value="<?= $pagingSystemEligibleModelIds[$i] ?>"><?= $pagingSystemEligibleModelNames[$i] ?></option>
+                                <?php endfor; ?>
+                            </select>
+                        </div>
+                    </div>
+                    <p class="help"><?= lang('PagingSystem.chosenPrimaryModelWillBeRoutedToTheFrontend') ?></p>
+                    <?php if ($primaryModelError): ?>
+                        <p class="help is-danger"><?= $primaryModelError ?></p>
+                    <?php endif; ?>
+                </div>
+            <?php endif ?>
+
+            <?php if ($eligibleAssetsModelExists): ?>
+                <!-- Assets model selection -->
+                <div class="field">
+                    <label class="label"><?= lang('PagingSystem.assets') ?></label>
+                    <div class="control">
+                        <div class="select">
+                            <select name="paging_system_assets_model_id">
+                                <?php for ($i = 0; $i < count($pagingSystemAssetsEligibleModelNames); $i++): ?>
+                                    <option value="<?= $pagingSystemAssetsEligibleModelIds[$i] ?>"><?= $pagingSystemAssetsEligibleModelNames[$i] ?></option>
+                                <?php endfor; ?>
+                            </select>
+                        </div>
+                    </div>
+                    <p class="help"><?= lang('PagingSystem.selectedModelWillServeAsPrimaryModelForServingAssets') ?></p>
+                    <?php if ($metaModelError): ?>
+                        <p class="help is-danger"><?= $metaModelError ?></p>
+                    <?php endif; ?>
+                </div>
+            <?php endif ?>
+
+            <?php if ($eligibleMetaModelExists): ?>
+                <!-- Meta model selection -->
+                <div class="field">
+                    <label class="label"><?= lang('PagingSystem.meta') ?></label>
+                    <div class="control">
+                        <div class="select">
+                            <select name="paging_system_meta_model_id">
+                                <?php for ($i = 0; $i < count($pagingSystemMetaEligibleModelNames); $i++): ?>
+                                    <option value="<?= $pagingSystemMetaEligibleModelIds[$i] ?>"><?= $pagingSystemMetaEligibleModelNames[$i] ?></option>
+                                <?php endfor; ?>
+                            </select>
+                        </div>
+                    </div>
+                    <p class="help"><?= lang('PagingSystem.selectedMetaModelWillBeUsedToInject') ?></p>
+                    <?php if ($metaModelError): ?>
+                        <p class="help is-danger"><?= $metaModelError ?></p>
+                    <?php endif; ?>
+                </div>
+            <?php endif ?>
+
+            <!-- Submit button -->
+            <div class="field is-grouped">
+                <div class="control is-flex-grow-1">
+                    <button type="submit" class="button is-primary">
+                        <span class="icon">
+                            <i class="fas fa-check"></i>
+                        </span>
+                        <span>
+                            <?= lang('Admin.save') ?>
+                        </span>
+                    </button>
                 </div>
             </div>
-            <p class="help"><?= lang('PagingSystem.chosenPrimaryModelWillBeRoutedToTheFrontend') ?></p>
-            <?php if ($primaryModelError): ?>
-                <p class="help is-danger"><?= $primaryModelError ?></p>
-            <?php endif; ?>
-        </div>
 
-        <!-- Assets model selection -->
-        <div class="field">
-            <label class="label"><?= lang('PagingSystem.assets') ?></label>
-            <div class="control">
-                <div class="select">
-                    <select name="paging_system_assets_model_id">
-                        <?php for ($i = 0; $i < count($pagingSystemAssetsEligibleModelNames); $i++): ?>
-                            <option value="<?= $pagingSystemAssetsEligibleModelIds[$i] ?>"><?= $pagingSystemAssetsEligibleModelNames[$i] ?></option>
-                        <?php endfor; ?>
-                    </select>
-                </div>
-            </div>
-            <p class="help"><?= lang('PagingSystem.selectedModelWillServeAsPrimaryModelForServingAssets') ?></p>
-            <?php if ($metaModelError): ?>
-                <p class="help is-danger"><?= $metaModelError ?></p>
-            <?php endif; ?>
-        </div>
-
-        <!-- Meta model selection -->
-        <div class="field">
-            <label class="label"><?= lang('PagingSystem.meta') ?></label>
-            <div class="control">
-                <div class="select">
-                    <select name="paging_system_meta_model_id">
-                        <?php for ($i = 0; $i < count($pagingSystemMetaEligibleModelNames); $i++): ?>
-                            <option value="<?= $pagingSystemMetaEligibleModelIds[$i] ?>"><?= $pagingSystemMetaEligibleModelNames[$i] ?></option>
-                        <?php endfor; ?>
-                    </select>
-                </div>
-            </div>
-            <p class="help"><?= lang('PagingSystem.selectedMetaModelWillBeUsedToInject') ?></p>
-            <?php if ($metaModelError): ?>
-                <p class="help is-danger"><?= $metaModelError ?></p>
-            <?php endif; ?>
-        </div>
-
-        <!-- Submit button -->
-        <div class="field is-grouped">
-            <div class="control is-flex-grow-1">
-                <button type="submit" class="button is-primary">
-                    <span class="icon">
-                        <i class="fas fa-check"></i>
-                    </span>
-                    <span>
-                        <?= lang('Admin.save') ?>
-                    </span>
-                </button>
-            </div>
-        </div>
-
-    </form>
+        </form>
+    <?php endif ?>
 </div>
 <?= $this->endSection() ?>
