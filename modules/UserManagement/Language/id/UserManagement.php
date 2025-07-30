3b23667244dfcb509bddeeeacd7a61a313624e61
damar <ibn.damr@gmail.com>
2025-07-30 22:11:22 +0700

    Added Indonesian language files for Admin, Paging System, User Management, and Voltic modules.

---
+++ modules/UserManagement/Language/id/UserManagement.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "moduleName" => "Pengguna",
+    "moduleDescription" => "Kelola identitas dan hak istimewa pengguna",
+
+    "userSavedSuccessfully" => "Pengguna berhasil disimpan",
+    "userDeletedSuccessfully" => "Pengguna berhasil dihapus",
+
+    'userForm' => 'Formulir Pengguna',
+    'leaveBlankToKeepCurrentPassword' => 'Biarkan kosong untuk mempertahankan kata sandi saat ini',
+    'groups' => 'Grup',
+];
